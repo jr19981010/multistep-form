b2d1b28f46dae5c150aa7f2fa4b6b3a8218a990a
jhayar <jhayarpilardirectly@gmail.com>
2023-09-22 15:42:14 +0800

    undo

--- src/components/SplitScreen/SplitScreen.jsx
+++ src/components/SplitScreen/SplitScreen.jsx
@@ -1,31 +1,31 @@
 import './SplitScreen.css';
 import React from 'react';
 import { useDisplay } from '../../DisplayContext';
-import { PersonalInfo } from '../personalInfo/PersonalInfo';
+
 export const SplitScreen = ({children}) =>{
     
     const {display} = useDisplay();
-    // let header = null;
-    // let main = null;
-    // let goBack = null;
-    // let nextStep = null;
+    let header = null;
+    let main = null;
+    let goBack = null;
+    let nextStep = null;
 
     children.forEach(child =>{
-        // if( child && child.type && child.type.name){
-        //     if( child.type.name === "Header"){
-        //         header = child;
-        //     }else if(child.type.name === "PersonalInfo" ||
-        //              child.type.name ==="SelectPlan" ||
-        //              child.type.name ==="PickAddOns" ||
-        //              child.type.name ==="FinishingUp"||
-        //              child.type.name ==="ThankYou"){
-        //         main = child;
-        //     }else if(child.type.name === "GoBack"){
-        //         goBack = child;
-        //     }else if(child.type.name === "NextStep"){
-        //         nextStep = child;
-        //     }
-        // }
+        if( child && child.type && child.type.name){
+            if( child.type.name === "Header"){
+                header = child;
+            }else if(child.type.name === "PersonalInfo" ||
+                     child.type.name ==="SelectPlan" ||
+                     child.type.name ==="PickAddOns" ||
+                     child.type.name ==="FinishingUp"||
+                     child.type.name ==="ThankYou"){
+                main = child;
+            }else if(child.type.name === "GoBack"){
+                goBack = child;
+            }else if(child.type.name === "NextStep"){
+                nextStep = child;
+            }
+        }
     })
 
     return(
@@ -33,14 +33,13 @@ export const SplitScreen = ({children}) =>{
 
         <div className="container">
             <div className="sidebar">
-                {/* {header} */}
-                <PersonalInfo />
+                {header}
             </div>
             <div className="main">
-            <PersonalInfo />
+            {main}
                 <div className={`controller ${display === "stepFive" ? "hide" : ""}`}>
-                <PersonalInfo />
-                <PersonalInfo />
+               {goBack}
+                {nextStep}
             </div>
             </div>
 
